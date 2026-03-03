8223eec5307d69e01c99b28be4140dce6def9557
natxsa-droid <natxsa@icloud.com>
2026-03-03 19:39:31 +0100

    1

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class Main {
+
+    public static void main (String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+
+}

79316f34a46fdcec7c90cd0bea89065eb823e7c3
natxsa-droid <natxsa@icloud.com>
2026-03-03 19:42:54 +0100

    1

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,18 @@ public class Main {
 
     public static void main (String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int broj = 0;
+        boolean tocanUnos = false;
+        while (tocanUnos) {
+            System.out.print("Unesite broj: ");
+            try {
+                broj = scanner.nextInt();
+                tocanUnos = true;
+            }
+            catch (InputMismatchException e) {
+                System.out.println("Nije unešen broj.");
+            }
+        }
 
 
     }
